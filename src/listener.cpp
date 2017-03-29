3e0a531ef6d5956efc5360f664d3ff6c173e8a16
ytlei <ytlei@umd.edu>
2017-03-28 22:10:24 -0500

    modify talker and listener

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,16 +1,28 @@
+/************************************************************
+ *                                                          *
+ * Copyright (C) 2017 by Yi-ting Lei                        *
+ *                                                          *
+ ***********************************************************/
+
+/**
+ *   @file	listener.cpp
+ *   @brief  	demo for ROS listener node
+ *
+ *   @author	Yi-ting Lei
+ *   @date	2017/3/28
+ */
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,13 +1,26 @@
+/************************************************************
+ *                                                          *
+ * Copyright (C) 2017 by Yi-ting Lei                        *
+ *                                                          *
+ ***********************************************************/
+
+/**
+ *   @file	talker.cpp
+ *   @brief  	this is the talker for ROS tutorial
+ *   
+ *
+ *   @author	Yi-ting Lei
+ *   @date	2017/3/28
+ */
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
 #include <sstream>
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -53,15 +66,14 @@ int main(int argc, char **argv)
    * a unique string for each message.
    */
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "This is a modified message " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
